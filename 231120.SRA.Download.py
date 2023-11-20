765939247e3015bd3568335b7b5cfe7aeac5d69a
ltk <xorrb85@gmail.com>
2023-11-20 13:30:57 +0900

    Fin

---
+++ 231120.SRA.Download.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import argparse
+#-------------------------------------------------------------------------#
+parser = argparse.ArgumentParser(description='Code Usage')
+parser.add_argument('1', metavar='<Tool>', help='Select download tool')
+parser.add_argument('2', metavar='<node>', help='Select node')
+parser.add_argument('3', metavar='<SRA num>', nargs='+', help='Set SRA number')
+args=parser.parse_args()
+#-------------------------------------------------------------------------#
+if os.path.isdir('00.RawData'):
+    pass
+else:
+    command = 'mkdir 00.RawData'
+    os.system(command)
+#-------------------------------------------------------------------------#
+SRA_list = sys.argv[3:]
+#-------------------------------------------------------------------------#
+if sys.argv[1] == 'fasterq-dump':
+    for sra in SRA_list:
+        command = f'mkdir -p 00.RawData/{sra}'
+        os.system(command)
+
+        with open(f'00.RawData/{sra}/job.sh', 'w') as note:
+            note.write('#!/bin/bash' + '\n' + '#' + '\n' + \
+                        '#SBATCH -J fasterq_dump' + '\n' + \
+                        '#SBATCH -o Log.%j.out' + '\n' + \
+                        f'#SBATCH --nodelist={sys.argv[2]}' + '\n' + \
+                        '#SBATCH -n 2' + '\n' + '\n' + \
+                        f'fasterq-dump -S -t TEMP/ -e 2 -O ../ {sra}')
+
+    with open('Total.Run.sh', 'w') as note:
+        for sra in SRA_list:
+            Path = os.getcwd()
+            note.write(f'cd {Path}/00.RawData/{sra}; sbatch job.sh' + '\n')
+
+elif sys.argv[1] == 'fastq-dump':
+    for sra in SRA_list:
+        command = f'mkdir -p 00.RawData/{sra}'
+        os.system(command)
+            
+        command = f'mkdir -p 00.RawData/{sra}'
+        os.system(command)
+
+        with open(f'00.RawData/{sra}/job.sh', 'w') as note:
+            note.write('#!/bin/bash' + '\n' + '#' + '\n' + \
+                        '#SBATCH -J fastq_dump' + '\n' + \
+                        '#SBATCH -o Log.%j.out' + '\n' + \
+                        f'#SBATCH --nodelist={sys.argv[2]}' + '\n' + \
+                        '#SBATCH -n 2' + '\n' + '\n' + \
+                        f'fastq-dump --split-files -gzip --outdir ../ {sra}')
+
+    with open('Total.Run.sh', 'w') as note:
+        for sra in SRA_list:
+            Path = os.getcwd()
+            note.write(f'cd {Path}/00.RawData/{sra}; sbatch job.sh' + '\n')
