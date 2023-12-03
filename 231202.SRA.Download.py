9891b336621da62a29dbc2ba05b18391caec4755
ltk <xorrb85@gmail.com>
2023-12-03 17:10:44 +0900

    Fin

--- 231202.SRA.Download.py
+++ 231202.SRA.Download.py
@@ -88,6 +88,61 @@ def CPU_MAX():
             else:
                 break
 
+    elif sys.argv[1] == 'fastq-dump' :
+        for sra in SRA_list:
+            Allocated_CPU += 2
+
+            command = f'mkdir -p 00.RawData/{sra}'
+            os.system(command)
+
+            with open(f'00.RawData/{sra}/job.sh', 'w') as note:
+                note.write('#!/bin/bash' + '\n' + '#' + '\n' + \
+                            '#SBATCH -J fasterq_dump' + '\n' + \
+                            f'#SBATCH -o 00.RawData/{sra}/Log.%j.out' + '\n' + \
+                            '#SBATCH --time=UNLIMITED' + '\n' + \
+                            f'#SBATCH --nodelist={sys.argv[2]}' + '\n' + \
+                            '#SBATCH -n 2' + '\n' + '\n' + \
+                            f'fastq-dump -S -t 00.RawData/{sra}/TEMP/ -e 8 -m 5000MB -O ../ {sra}' + '\n' + \
+                            f'echo {sra} Raw data download is finished.\nPlease check log file. > 01.Finish/{sra}.txt')
+            
+            command = f'sbatch 00.RawData/{sra}/job.sh'
+            os.system(command)
+
+            if Allocated_CPU + 2 > MAX_CPU:
+                break
+
+        time.sleep(3600)
+
+        Scheduled_SRA = SRA_list[int(MAX_CPU/2) : ]
+
+        while True:
+            Finished_SRA = os.listdir('01.Finish/')
+            if len(Scheduled_SRA) != 0:
+                for index in range(0, len(Finished_SRA)):
+                    command = f'mkdir -p 00.RawData/{Scheduled_SRA[index]}'
+                    os.system(command)
+
+                    with open(f'00.RawData/{Scheduled_SRA[index]}/job.sh', 'w') as note:
+                        note.write('#!/bin/bash' + '\n' + '#' + '\n' + \
+                                    '#SBATCH -J fasterq_dump' + '\n' + \
+                                    f'#SBATCH -o 00.RawData/{Scheduled_SRA[index]}/Log.%j.out' + '\n' + \
+                                    '#SBATCH --time=UNLIMITED' + '\n' + \
+                                    f'#SBATCH --nodelist={sys.argv[2]}' + '\n' + \
+                                    '#SBATCH -n 2' + '\n' + '\n' + \
+                                    f'fasterq-dump -S -t 00.RawData/{Scheduled_SRA[index]}/TEMP/ -e 8 -m 5000MB -O ../ {Scheduled_SRA[index]}' + '\n' + \
+                                    f'echo {Scheduled_SRA[index]} Raw data download is finished.\nPlease check log file. > 01.Finish/{Scheduled_SRA[index]}.txt')
+
+                    command = f'sbatch 00.RawData/{Scheduled_SRA[index]}/job.sh'
+                    os.system(command)
+
+                for index in range(0, len(Finished_SRA)):
+                    Scheduled_SRA.pop(index)
+                    os.remove('01.Finish/' + Finished_SRA[index])
+
+                time.sleep(3600)
+            else:
+                break
+
 def CPU_free():
     if sys.argv[1] == 'fasterq-dump':
         for sra in SRA_list:
