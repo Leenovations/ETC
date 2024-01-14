b64b50c18771432041f675b4c9f3658c15fa411e
ltk <xorrb85@gmail.com>
2024-01-14 14:27:44 +0900

    Update

--- 240114.M.GeneCNV.Range.py
+++ 240114.M.GeneCNV.Range.py
@@ -15,4 +15,4 @@ with open("/media/src/hg19/08.bed/NCBI.RefSeq.Selected.Exon.Chr.X.bed", "r") as 
             Strand = splitted[5]
 
             for position in range(Start, End + 1):
-                note.write(Chr + '\t' + str(position) + '\t' + str(position) + '\t' + Gene + '\t' + Exon + '\t' + Strand + '\n')
+                note.write(Chr + '\t' + str(position - 1) + '\t' + str(position + 1) + '\t' + Gene + '\t' + Exon + '\t' + Strand + '\n')
