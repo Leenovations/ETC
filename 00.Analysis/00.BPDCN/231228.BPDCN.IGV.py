185c9db1aea70bb60253e60bc94994b2eae0151c
ltk <xorrb85@gmail.com>
2024-01-15 14:02:47 +0900

    Update

--- 00.Analysis/00.BPDCN/231228.BPDCN.IGV.py
+++ 00.Analysis/00.BPDCN/231228.BPDCN.IGV.py
@@ -7,7 +7,7 @@ Sample = glob.glob('*.results.xlsx')
 Sample.sort()
 Sample = [sample.split('.')[0] for sample in Sample]
 #--------------------------------------------------------------------------------------#
-for sample in Sample:
+for sample in Sample[0:1]:
     if os.path.isdir(f'{sample}_IGV'):
         pass
     else:
@@ -16,9 +16,10 @@ for sample in Sample:
 
     Result = pd.read_excel(f'{sample}.results.xlsx', 
                            engine='openpyxl',
-                           sheet_name='All.Path.VUS')
-    Result = Result[(Result.iloc[:, 1] == 'O') & (Result.iloc[:, 2] != 'T3')]
+                           sheet_name='Pathogenic.VUS')
+    Result = Result[Result['main.accession'] == 'O']
     Result = Result.dropna(subset=['Tier'])
+    Result = Result.dropna(subset=['exon'])
 
     if Result.empty:
         continue
@@ -32,10 +33,12 @@ for sample in Sample:
             Position = Result.iloc[row, 4]
             Splitted = Position.split(':')
             Chr = str(Splitted[0])
-            Start = int(Splitted[1].split('-')[0])
-            End = int(Splitted[1].split('-')[1])
+            Start = int(Splitted[1].split('-')[0]) - 10
+            End = int(Splitted[1].split('-')[1]) + 10
             Gene = Result.iloc[row, 5]
             Variant = Result.iloc[row, 8]
+            Variant = Variant.replace('>','_')
+            Variant = Variant.replace('*','_')
             VAF = Result.iloc[row, 13]
 
             ir.set_saveopts(img_dir = f'{sample}_IGV/', img_basename = f'{sample}.{Gene}.{Variant}.{VAF}.png')
