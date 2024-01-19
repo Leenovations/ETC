15bb890c189089ea6ac0bfec3af563cd061094e0
ltk <xorrb85@gmail.com>
2024-01-19 13:01:51 +0900

    Update

--- 00.Analysis/01.AML/240119.a.AML.MethylPercent.py
+++ 00.Analysis/01.AML/240119.a.AML.MethylPercent.py
@@ -47,8 +47,27 @@ for i in range(4, len(df_filtered.columns), 2):
     DATA[df_filtered.columns[i].replace('_Cov', '')] = round(df_filtered.iloc[:, i+1] / df_filtered.iloc[:, i] * 100, 2)
 #----------------------------------------------------------------------------------------#
 Merged = pd.concat([Data.iloc[:,0:4], DATA], axis=1)
-Merged.to_csv('/media/node01-HDD01/00.AML/01.Results/240119.AML.150bp.Methyl.txt',
+Merged.to_csv('/media/node01-HDD01/00.AML/01.Results/240119.AML.150bp.test.Methyl.txt',
               sep='\t',
-              header='inder',
+              header='infer',
+              index=False)
+#----------------------------------------------------------------------------------------#
+CR = round(Merged.iloc[:, 4:16].mean(axis=1), 2)
+NR = round(Merged.iloc[:, 16:].mean(axis=1), 2)
+Mean = pd.concat([Data.iloc[:,0:4], CR, NR], axis=1)
+Mean.columns = ['Chromosome', 'Start', 'End', 'Strand', 'CR', 'NR']
+Mean.to_csv('/media/node01-HDD01/00.AML/01.Results/240119.AML.150bp.Mean.Methyl.txt',
+              sep='\t',
+              header='infer',
+              index=False)
+#----------------------------------------------------------------------------------------#
+CR = round(Merged.iloc[:, 4:16].mean(axis=1), 2)
+NR = round(Merged.iloc[:, 16:].mean(axis=1), 2)
+Diff = round(CR - NR, 2)
+Diff = pd.concat([Data.iloc[:,0:4], Diff], axis=1)
+Diff.columns = ['Chromosome', 'Start', 'End', 'Strand', 'CR-NR']
+Diff.to_csv('/media/node01-HDD01/00.AML/01.Results/240119.AML.150bp.Diff.Methyl.txt',
+              sep='\t',
+              header='infer',
               index=False)
 #----------------------------------------------------------------------------------------#
