2e57c0c0583ab0552500fe449c6044038cee7688
ltk <xorrb85@gmail.com>
2024-01-19 12:35:19 +0900

    Update

---
+++ 00.Analysis/01.AML/240119.a.AML.MethylPercent.py
@@ -0,0 +1,50 @@
+#!/home/lab/anaconda3/envs/NGS/bin/python3
+
+import argparse
+import pandas as pd
+import numpy as np
+#----------------------------------------------------------------------------------------#
+Sample = ['CR_AYB',
+            'CR_HJJ',
+            'CR_JJB',
+            'CR_JTN',
+            'CR_KDS',
+            'CR_KYK',
+            'CR_LJP',
+            'CR_LKB',
+            'CR_LKH',
+            'CR_NKYY',
+            'CR_SYK',
+            'CR_YSJ',
+            'NR_HSW',
+            'NR_JMJ',
+            'NR_JWS',
+            'NR_KKY',
+            'NR_KWH',
+            'NR_KYS',
+            'NR_LHB',
+            'NR_LMS',
+            'NR_PHS',
+            'NR_PMK',
+            'NR_SWSJ',
+            'NR_YDM']
+#----------------------------------------------------------------------------------------#
+SAMPLE = ['Chromosome', 'Start', 'End', 'Strand']
+for samp in Sample:
+    SAMPLE.append(samp + '_Cov')
+    SAMPLE.append(samp + '_Cs')
+    SAMPLE.append(samp + '_Ts')
+#----------------------------------------------------------------------------------------#
+Data = pd.read_csv('/media/node01-HDD01/00.AML/01.Results/240119.AML.test.150bp.txt',
+                   sep='\t',
+                   header=None,
+                   names=SAMPLE)
+#----------------------------------------------------------------------------------------#
+df_filtered = Data.drop(columns=[col for col in Data.columns if '_Ts' in col])
+#----------------------------------------------------------------------------------------#
+DATA = pd.DataFrame()
+for i in range(4, len(df_filtered.columns), 2):
+    DATA[df_filtered.columns[i].replace('_Cov', '')] = round(df_filtered.iloc[:, i+1] / df_filtered.iloc[:, i] * 100, 2)
+
+Merged = pd.concat([Data.iloc[:,0:4], DATA], axis=1)
+print(Merged)
