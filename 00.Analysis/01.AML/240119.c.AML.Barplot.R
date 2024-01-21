f72343726ae46a9247014d86fcfcaee74b01f27a
ltk <xorrb85@gmail.com>
2024-01-21 17:19:08 +0900

    Update

--- 00.Analysis/01.AML/240119.c.AML.Barplot.R
+++ 00.Analysis/01.AML/240119.c.AML.Barplot.R
@@ -7,8 +7,8 @@ library(GenomicRanges)
 library(ggpubr)
 library(openxlsx)
 
-# Gene <- c('TAL1')
 Gene <- c('TAL1', 'BCL11A', 'GATA1', 'GATA2', 'NFE2')
+DMR_Data <- read.xlsx('/labmed/11.AML/230625.AML.Total.Data.xlsx', sheet='Methylkit')
 
 for (gene in Gene){
   data <- read.table(paste0('/media/node01-HDD01/00.AML/01.Results/240119.AML.150bp.', gene, '.Methyl.txt'),
@@ -16,7 +16,7 @@ for (gene in Gene){
                      header=TRUE)
   
   data$color <- ifelse(data$CR.NR > 0, 'tan1', 'lightskyblue')
-  
+  #---------------------------------------------------------------------------------------------------------#
   CR <- ggplot(data, aes(x = Start, y = CR)) +
     geom_bar(fill = 'tan1',
              color=NA,
@@ -28,7 +28,7 @@ for (gene in Gene){
     ylab('') + 
     ylim(0, 101) + 
     ggtitle(paste0('', '\n'), subtitle='CR') +
-    scale_y_continuous(breaks = c(0, 100), labels=c(0, 100), expand = c(0, 1)) + 
+    scale_y_continuous(breaks = c(0, 100), labels=c(0, 100)) + 
     theme(axis.line.x = element_blank(),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
@@ -39,7 +39,7 @@ for (gene in Gene){
           axis.text=element_text(color="black"),
           axis.ticks.x=element_blank(),
           axis.text.x=element_blank())
-  
+  #---------------------------------------------------------------------------------------------------------#
   NR <- ggplot(data, aes(x = Start, y = NR)) +
     geom_bar(fill = 'lightskyblue',
              color=NA,
@@ -50,7 +50,7 @@ for (gene in Gene){
     xlab('') + 
     ylab('') + 
     ggtitle('NR') + 
-    scale_y_continuous(breaks = c(0, 100), labels=c(0, 100), expand = c(0, 1)) + 
+    scale_y_continuous(breaks = c(0, 100), labels=c(0, 100)) + 
     theme(axis.line.x = element_blank(),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
@@ -60,7 +60,24 @@ for (gene in Gene){
           axis.text=element_text(color="black"),
           axis.ticks.x=element_blank(),
           axis.text.x=element_blank())
-  
+  #---------------------------------------------------------------------------------------------------------#
+  Data_sub <- subset(DMR_Data, DMR_Data$annot.symbol==gene)
+  Range <- unique(Data_sub[,c('start', 'end')])
+  #---------------------------------------------------------------------------------------------------------#
+  bed1 <- read.table("/media/src/hg19/01.Methylation/00.Bed/CGI.bed", header = FALSE, col.names = c("chromosome", "start", "end"))
+  bed2 <- read.table("/media/src/hg19/01.Methylation/00.Bed/NCBI.RefSeq.Selected.Gene.bed", header = FALSE, col.names = c("chromosome", "start", "end", "genesymbol", "strand"))
+  bed2 <- subset(bed2, bed2$genesymbol==gene)
+  gr1 <- GRanges(seqnames = bed1$chromosome, ranges = IRanges(start = bed1$start, end = bed1$end))
+  gr2 <- GRanges(seqnames = bed2$chromosome, ranges = IRanges(start = bed2$start, end = bed2$end))
+  overlap <- findOverlaps(gr2, gr1)
+  CpG_island_table <- data.frame('Start'=bed1[overlap@to,]$start,
+                                 'End'=bed1[overlap@to,]$end)
+  #---------------------------------------------------------------------------------------------------------#
+  Tx <- read.table('/media/src/hg19/01.Methylation/00.Bed/NCBI.RefSeq.Selected.Exon.Intron.None.bed', sep='\t')
+  colnames(Tx) <- c('Chromosome', 'Start' ,'End', 'GeneSymbol', 'Region', 'Strand')
+  Exon <- subset(Tx, Tx$GeneSymbol == gene & Tx$Region == 'exon')
+  Intron <- subset(Tx, Tx$GeneSymbol == gene & Tx$Region == 'intron')
+  #---------------------------------------------------------------------------------------------------------#
   CR_NR <- ggplot(data, aes(x = Start, y = CR-NR)) +
     geom_bar(fill = data$color,
              color=NA,
@@ -76,53 +93,22 @@ for (gene in Gene){
                color='black',
                alpha=0.3,
                size=0.1) + 
-    theme(panel.grid.major = element_blank(),
+    theme(plot.margin = unit(c(0, 0, 2, 0), "cm"),
+          axis.line.x = element_blank(),
+          panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
           axis.title.y = element_text(size=10),
           plot.title = element_text(size=10, hjust=0.5, face = c('bold')),
           axis.title = element_text(size = 10),
           axis.text=element_text(color="black"),
           axis.ticks.x=element_blank(),
-          axis.text.x=element_blank())
-  
-  Methyl <- CR + NR + CR_NR + plot_layout(ncol = 1)
-
-  Tx <- read.table('/media/src/hg19/01.Methylation/00.Bed/NCBI.RefSeq.Selected.Exon.Intron.None.bed', sep='\t')
-  colnames(Tx) <- c('Chromosome', 'Start' ,'End', 'GeneSymbol', 'Region', 'Strand')
-  Table <- subset(Tx, Tx$GeneSymbol == gene)
-  Table['Color'] = ifelse(Table$Region=='exon', "gray30", "gray30")
-  Table['Size'] = ifelse(Table$Region=='exon', 1, 0.5)
-  
-  Exon_Intron <- ggplot(Table) +
-    geom_segment(aes(x = Start, xend = End,
-                     y = 4, yend = 4,
-                     colour = Color, size= Size)) +
-    scale_color_manual(values = c("gray30", 'gray30'), 
-                       labels = c("exon", "intron")) + 
-    scale_fill_manual(values = c("gray30", 'gray30'), 
-                       labels = c("exon", "intron")) + 
-    ylim(0,5) +
-    xlab('') + ylab('Percent') +
-    theme_bw() +
-    theme(panel.background = element_blank(),
-          panel.border = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          axis.text.x = element_blank(),
-          axis.title.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.title.y = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.ticks.y = element_blank(),
-          legend.background = element_blank(),
-          legend.position = "none",
-          legend.key = element_blank(),
-          legend.text = element_blank(),
-          legend.title = element_blank())
-  
-  Plot <- Methyl + Exon_Intron +
-    plot_layout(ncol = 1)
-  
+          axis.text.x=element_blank()) +
+    annotate(geom = "rect", xmin=Range$start, xmax=Range$end, ymin=-65 , ymax=-70, color="blueviolet", fill='blueviolet') +
+    annotate(geom = "rect", xmin=CpG_island_table$Start, xmax=CpG_island_table$End, ymin=-75 , ymax=-80, color="darkgreen", fill='darkgreen') +
+    annotate(geom = "rect", xmin=Exon$Start, xmax=Exon$End, ymin=-90 , ymax=-110, color="gray30", fill='gray30') +
+    annotate(geom = "rect", xmin=Intron$Start, xmax=Intron$End, ymin=-99 , ymax=-101, color="gray30", fill='gray30') +
+    coord_cartesian(ylim = c(-50, 50), expand = T, clip = "off")
+  #---------------------------------------------------------------------------------------------------------#
   Data <- read.xlsx('/labmed/11.AML/230625.AML.Total.Data.xlsx', sheet='Norm.Over.50')
 
   Sub_Data <- subset(Data, Data$Gene == gene)
@@ -151,13 +137,15 @@ for (gene in Gene){
           legend.text = element_blank(),
           legend.title = element_blank())
 
-  BAR <- plot_grid(Plot, Barplot)
-  
-  grid_with_title <- ggdraw() +
-    draw_plot(BAR) +
+  #---------------------------------------------------------------------------------------------------------#
+      Methyl <- CR + NR + CR_NR + plot_layout(ncol = 1)
+      Plot <- plot_grid(Methyl, Barplot)
+
+  Merged_plot <- ggdraw() +
+    draw_plot(Plot) +
     draw_label(gene, size = 15, x = 0.5, y = 0.93, fontface = "bold.italic")
-  
+  #---------------------------------------------------------------------------------------------------------#
   ggsave(paste0('/labmed/11.AML/', gene, '.Exon.Bar.png'),
          height=5,
-         plot=grid_with_title)
+         plot=Merged_plot)
 }
