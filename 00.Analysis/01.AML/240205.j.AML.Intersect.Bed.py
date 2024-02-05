6348ea8a52c55a35f3ea367991648ccbc5de6ee7
ltk <xorrb85@gmail.com>
2024-02-05 17:16:59 +0900

    Update

--- 00.Analysis/01.AML/240205.j.AML.Intersect.Bed.py
+++ 00.Analysis/01.AML/240205.j.AML.Intersect.Bed.py
@@ -11,31 +11,35 @@ Header_WGBS = Header['WGBS']
 Header_WGBS = Header_WGBS.dropna()
 Header_WGBS = Header_WGBS.to_list()
 #-------------------------------------------------------------------------------------#
-Bed = pd.read_csv('/media/src/hg19/01.Methylation/00.Bed/CGI.bed',
-                sep='\t',
-                names=['Chromosome', 'Start', 'End'])
-pyBed = pr.PyRanges(Bed)
-#-------------------------------------------------------------------------------------#
-Data = pd.read_csv('/labmed/01.AML/01.WGBS/240119.AML.150bp.Methyl.txt',
-                   sep='\t',
-                   low_memory=False)
-Data.columns = ['Chromosome', 'Start', 'End', 'Strand'] + Header_WGBS
+def Cal(bed, prefix, *colnames):
+    columnnames = list(colnames)
 
-pyData = pr.PyRanges(Data)
+    Bed = pd.read_csv(bed,
+                    sep='\t',
+                    names=columnnames)
+    pyBed = pr.PyRanges(Bed)
+    #-------------------------------------------------------------------------------------#
+    Data = pd.read_csv('/labmed/01.AML/01.WGBS/240119.AML.150bp.Methyl.txt',
+                    sep='\t',
+                    low_memory=False)
+    Data.columns = ['Chromosome', 'Start', 'End', 'Strand'] + Header_WGBS
 
-Intersect = pyData.join(pyBed).df
-Intersect = Intersect.drop(['Start_b', 'End_b'], axis=1)
-Intersect = Intersect.astype({'Chromosome': 'str', 'Start' : 'int', 'End' : 'int', 'Strand' : 'str'})
-Intersect = round(Intersect.groupby(['Chromosome', 'Start', 'End', 'Strand']).mean(), 2)
-Intersect = Intersect.fillna('NaN')
+    pyData = pr.PyRanges(Data)
 
-Intersect.to_csv(f'/labmed/01.AML/01.WGBS/240205.AML.150bp.CGI.Methyl.txt',
-                sep='\t',
-                index=False)
+    Intersect = pyBed.join(pyData).df
+    Intersect = Intersect.drop(['Start_b', 'End_b'], axis=1)
+    Intersect = Intersect.astype({'Chromosome': 'str', 'Start' : 'int', 'End' : 'int', 'Strand' : 'str'})
+    Intersect = Intersect.groupby(['Chromosome', 'Start', 'End', 'Strand'], as_index=False).mean().round(2)
+    Intersect = Intersect.fillna('NaN')
 
-Intersect_mean = Intersect.mean()
-Intersect_mean.to_csv(f'/labmed/01.AML/01.WGBS/240205.AML.150bp.CGI.Mean.Methyl.txt',
-                      sep='\t',
-                      header='infer',
-                      index=False)
-#-------------------------------------------------------------------------------------#
+    Intersect.to_csv(f'/labmed/01.AML/01.WGBS/240205.AML.150bp.{prefix}.Methyl.txt',
+                    sep='\t',
+                    index=False)
+
+    Intersect_mean = Intersect.mean()
+    Intersect_mean.to_csv(f'/labmed/01.AML/01.WGBS/240205.AML.150bp.{prefix}.Mean.Methyl.txt',
+                        sep='\t',
+                        header='infer',
+                        index=False)
+    #-------------------------------------------------------------------------------------#
+Cal('/media/src/hg19/01.Methylation/00.Bed/100000bp.bed', '100kb', 'Chromosome', 'Start', 'End')
