1ba72f5f919cd729e16f92c57f6492bf7658311c
LTK <xorrb85@gmail.com>
2024-02-12 18:51:44 +0900

    Update

--- 231214.SampleSheet.py
+++ 231214.SampleSheet.py
@@ -1,63 +1,84 @@
 #!/usr/bin/python3
 import sys
 import os
-import glob
 
 LIST = sys.argv[1:]
 
 with open('SampleSheet.txt', 'w') as note1:
-	for fastq in LIST:
-		if '_R1' in fastq:
-			Name = fastq.split('/')[-1]
-			Name = Name.split('_R1')[0]
-			command = f'mkdir {Name}'
-			os.system(command)
-			Size = os.path.getsize(fastq)
+	for data in LIST:
+		if data.split('.')[-1] == 'data' or data.split('.')[-1] == 'gz':
+			if '_R1' in data:
+				Name = data.split('/')[-1]
+				Name = Name.split('_R1')[0]
+				command = f'mkdir {Name}'
+				os.system(command)
+				Size = os.path.getsize(data)
 
-			name = fastq.split('_R1')[0]
-			First = fastq
-			Second = fastq.replace('_R1', '_R2')
-			note1.write(Name + '\t' + First+ '\t' + Second + '\t' + str(Size) + '\n')
+				name = data.split('_R1')[0]
+				First = data
+				Second = data.replace('_R1', '_R2')
+				note1.write(Name + '\t' + First+ '\t' + Second + '\t' + str(Size) + '\n')
+
+				with open(f'{Name}/SampleSheet.txt', 'w') as note2:
+					name = data.split('_R1')[0]
+					First = data
+					Second = data.replace('_R1', '_R2')
+					note2.write(Name + '\t' + First+ '\t' + Second + '\n')
+
+			elif '_1.data.gz' in data:
+				Name = data.split('/')[-1]
+				Name = Name.split('_1.data.gz')[0]
+				command = f'mkdir {Name}'
+				os.system(command)
+				Size = os.path.getsize(data)
+
+				name = data.split('_1.data.gz')[0]
+				First = data
+				Second = data.replace('_1.data.gz', '_2.data.gz')
+				note1.write(Name + '\t' + First+ '\t' + Second + '\t' + str(Size) + '\n')
+
+				with open(f'{Name}/SampleSheet.txt', 'w') as note2:
+					name = data.split('_1.data.gz')[0]
+					First = data
+					Second = data.replace('_1.data.gz', '_2.data.gz')
+					note2.write(Name + '\t' + First+ '\t' + Second + '\n')
+
+			elif '_1.data' in data:
+				Name = data.split('/')[-1]
+				Name = Name.split('_1.data')[0]
+				command = f'mkdir {Name}'
+				os.system(command)
+				Size = os.path.getsize(data)
+
+				name = data.split('_1.data')[0]
+				First = data
+				Second = data.replace('_1.data', '_2.data')
+				note1.write(Name + '\t' + First+ '\t' + Second + '\t' + str(Size) + '\n')
+
+				with open(f'{Name}/SampleSheet.txt', 'w') as note2:
+					name = data.split('_1.data')[0]
+					First = data
+					Second = data.replace('_1.data', '_2.data')
+					note2.write(Name + '\t' + First+ '\t' + Second + '\n')
+
+		elif data.split('.')[-1] == 'bam':
+			Name = data.split('/')[-1]
+			Name = Name.split('.bam')[0]
+			Bam = data
+
+			command = f'mkdir -p {Name}/03.Align/'
+			os.system(command)
+
+			command = f'mv {Bam} {Name}/03.Align/'
+			os.system(command)
+			command = f'mv {Bam}.bai {Name}/03.Align/'
+			os.system(command)
+
+			note1.write(Name + '\t' + Name + '\t' + Name + '\n')
 
 			with open(f'{Name}/SampleSheet.txt', 'w') as note2:
-				name = fastq.split('_R1')[0]
-				First = fastq
-				Second = fastq.replace('_R1', '_R2')
-				note2.write(Name + '\t' + First+ '\t' + Second + '\n')
+				Name = data.split('/')[-1]
+				Name = Name.split('.bam')[0]
+				Bam = data
 
-		elif '_1.fastq.gz' in fastq:
-			Name = fastq.split('/')[-1]
-			Name = Name.split('_1.fastq.gz')[0]
-			command = f'mkdir {Name}'
-			os.system(command)
-			Size = os.path.getsize(fastq)
-
-			name = fastq.split('_1.fastq.gz')[0]
-			First = fastq
-			Second = fastq.replace('_1.fastq.gz', '_2.fastq.gz')
-			note1.write(Name + '\t' + First+ '\t' + Second + '\t' + str(Size) + '\n')
-
-			with open(f'{Name}/SampleSheet.txt', 'w') as note2:
-				name = fastq.split('_1.fastq.gz')[0]
-				First = fastq
-				Second = fastq.replace('_1.fastq.gz', '_2.fastq.gz')
-				note2.write(Name + '\t' + First+ '\t' + Second + '\n')
-
-		elif '_1.fastq' in fastq:
-			Name = fastq.split('/')[-1]
-			Name = Name.split('_1.fastq')[0]
-			command = f'mkdir {Name}'
-			os.system(command)
-			Size = os.path.getsize(fastq)
-
-			name = fastq.split('_1.fastq')[0]
-			First = fastq
-			Second = fastq.replace('_1.fastq', '_2.fastq')
-			note1.write(Name + '\t' + First+ '\t' + Second + '\t' + str(Size) + '\n')
-
-			with open(f'{Name}/SampleSheet.txt', 'w') as note2:
-				name = fastq.split('_1.fastq')[0]
-				First = fastq
-				Second = fastq.replace('_1.fastq', '_2.fastq')
-				note2.write(Name + '\t' + First+ '\t' + Second + '\n')
-
+				note2.write(Name + '\t' + Name + '\t' + Name + '\n')
